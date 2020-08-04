7a935bb25fa6e09a81cbbf9f067db3c63fa1fbfe
馒头饭 <mhjlw@126.com>
2020-08-04 11:20:27 +0800

    :new:第三方登录插件

---
+++ thirdpartylogin/_controller_/thirdpartylogin/index/Auth.php
@@ -0,0 +1,85 @@
+<?php
+namespace app\plugins\thirdpartylogin\index;
+use anerg\OAuth2\OAuth;
+use think\Controller;
+// use think\facade\Config;
+use app\service\UserService;
+use app\service\PluginsService;
+use app\plugins\thirdpartylogin\service\Service;
+
+class Auth extends Controller
+{
+    private $config = array();
+    public function bind($params = [])
+    {
+        $party = $params['party'];
+        $data = Service::loadData();
+        if ($data) {
+            $config = Service::config();
+            $this->config = array(
+                'response_type' => 'code',
+                'app_id' => $data[$party . '_appid'],
+                'callback' => Service::PluginsHomeUrl(PluginsHomeUrl('thirdpartylogin', 'auth', 'callback', array('party' => $party))),
+                'scope' => $config[$party]['scope']
+            );
+            if (isMobile()) {
+                return redirect(OAuth::$party($this->config)->setDisplay('mobile')->getRedirectUrl());
+            } else {
+                return redirect(OAuth::$party($this->config)->getRedirectUrl());
+            } 
+        }
+    }
+    public function unbind($params = [])
+    {
+        $party = $params['party'];
+        $db = $params['db'];
+        $user = UserService::LoginUserInfo();
+        if (!empty($user)) {
+            return Service::do('unbind', array(
+                'channel' => $party,
+                'id' => $user['id'],
+                'db' => $db
+            ));
+        } else {
+            return DataReturn('解绑失败', -100);
+        }
+    }
+    public function callback($params = [])
+    {
+        $party = $params['party'];
+        $data = Service::loadData();
+        if ($data) {
+            $this->config = array(
+                'app_id' => $data[$party . '_appid'],
+                'app_secret' => isset($data[$party . '_secret']) ? $data[$party . '_secret'] : '',
+                'callback' => Service::PluginsHomeUrl(PluginsHomeUrl('thirdpartylogin', 'auth', 'callback', array('party' => $party)), $party),
+                'grant_type'    => 'authorization_code',
+                'withUnionid' => 'true'
+            );
+            if ($party === 'alipay') {
+                $this->config = array_merge($this->config, array(
+                    'pem_private' => $data[$party . '_pem_private'],
+                    'pem_public' => $data[$party . '_pem_public']
+                ));
+            }
+            //获取格式化后的第三方用户信息
+            $snsInfo = OAuth::$party($this->config)->userinfo();
+            // 获取登录用户
+            $user = UserService::LoginUserInfo();
+            if (!empty($user)) { // 如果已经登陆了开始绑定流程
+                Service::do('bind', array_merge($snsInfo, array('id' => $user['id'])));
+                return redirect(MyUrl('index/personal/index'));
+            } else {
+                if (Service::do('find', $snsInfo) === false) {
+                    $this->assign('plugin_thirdpartylogin_b', base64_encode(http_build_query($snsInfo)));
+                    return $this->fetch('../../../plugins/view/thirdpartylogin/public/index/callback');
+                } else {
+                    $last_url = session(Service::$last_url);
+                    session(Service::$last_url, null);
+                    return redirect($last_url ? $last_url : '/');
+                }
+            } 
+        }
+    }
+}
+?>
---
+++ thirdpartylogin/_controller_/thirdpartylogin/wga/WGA.souce.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\plugins\thirdpartylogin\wga;
+use app\service\PluginsService;
+use app\plugins\thirdpartylogin\service\Service;
+class WGA
+{
+    public static function save($params = [])
+    {
+        $r = @file_get_contents('https://k.os120.com/api/wga/verify?out_type=json&des=正版验证&domain=' . $_SERVER['SERVER_NAME'], false, stream_context_create(array('http' => array('method' => "GET",'timeout' => 3))));
+        if ($r) {
+            $r = json_decode($r, true);
+            if (isset($r['code']) && $r['code'] === -1) {
+                return DataReturn($r['msg'], -1);
+            }
+        }
+        Service::savePayConfig(array('Alipay'=>array(
+            'appid' => $params['alipay_appid'],
+            'rsa_private' => $params['alipay_rsa_private'],
+            'rsa_public' => $params['alipay_rsa_public'],
+            'out_rsa_public' => $params['alipay_out_rsa_public'],
+        )));
+        unset($params['alipay_appid'], $params['alipay_rsa_private'], $params['alipay_rsa_public'], $params['alipay_out_rsa_public']);
+        return PluginsService::PluginsDataSave(['plugins'=>'thirdpartylogin', 'data'=>$params]);
+    }
+}
+?>
