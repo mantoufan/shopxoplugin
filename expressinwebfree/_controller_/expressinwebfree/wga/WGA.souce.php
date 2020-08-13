9a541106b75050b5784eacd60c007b5fae8dc849
馒头饭 <mhjlw@126.com>
2020-08-13 16:44:39 +0800

    :new:update the wga url

--- expressinwebfree/_controller_/expressinwebfree/wga/WGA.souce.php
+++ expressinwebfree/_controller_/expressinwebfree/wga/WGA.souce.php
@@ -8,7 +8,7 @@ use app\plugins\expressinwebfree\service\Service;
 class WGA extends Controller
 {
     private static function wga() {
-        $r = @file_get_contents('https://k.os120.com/api/wga/verify?out_type=json&name=shopxoplugin_expressinwebfree&des=正版验证&domain=' . $_SERVER['SERVER_NAME'], false, stream_context_create(array('http' => array('method' => "GET",'timeout' => 3))));
+        $r = @file_get_contents('https://api.os120.com/wga/verify?out_type=json&name=shopxoplugin_expressinwebfree&des=正版验证&domain=' . $_SERVER['SERVER_NAME'], false, stream_context_create(array('http' => array('method' => "GET",'timeout' => 3))));
         if ($r) {
             $r = json_decode($r, true);
             if (isset($r['code']) && $r['code'] === -1) {
