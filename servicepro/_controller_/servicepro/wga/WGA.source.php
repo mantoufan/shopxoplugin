73d5f39b80eacca0ee93aa1c94e3b76595b355c6
馒头饭 <mhjlw@126.com>
2020-09-04 13:40:06 +0800

    :new:更新客服插件的自动更新系统

--- servicepro/_controller_/servicepro/wga/WGA.source.php
+++ servicepro/_controller_/servicepro/wga/WGA.source.php
@@ -4,17 +4,18 @@ use app\service\PluginsService;
 class WGA
 {
     private static function wga() {
-        $config = json_decode(file_get_contents('../config.json'), true); 
+        $root = dirname(__FILE__);
+        $config = json_decode(file_get_contents($root . '/../config.json'), true); 
         $r = @file_get_contents('https://api.os120.com/wga/verify?out_type=json&name=shopxoplugin_' . $config['base']['plugins'] . '&version=' . $config['base']['version'] . '&des=正版验证&domain=' . $_SERVER['SERVER_NAME'], false, stream_context_create(array('http' => array('method' => "GET",'timeout' => 3))));
         if ($r) {
             $r = json_decode($r, true);
             if (isset($r['code']) && $r['code'] === -1) {
-                return DataReturn($r['msg'], -1);
+                return $r['msg'];
             }
-            if (isset($r['tip']) && !empty($r['tip'])) {
-                file_put_contents(dirname(__FILE__) . '/wga_tip.txt', $r['tip']);
-            } else if (file_exists(dirname(__FILE__) . '/wga_tip.txt')){
-                unlink(dirname(__FILE__) . '/wga_tip.txt');
+            if (isset($r['data']) && isset($r['data']['tip']) && !empty($r['data']['tip'])) {
+                file_put_contents($root . '/wga_tip.txt', $r['data']['tip']);
+            } else if (file_exists($root . '/wga_tip.txt')){
+                unlink($root. '/wga_tip.txt');
             }
         }
         return false;
