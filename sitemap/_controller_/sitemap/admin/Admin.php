7a6546965103e869fab6ff2c6ea72a9d592e3d01
馒头饭 <mhjlw@126.com>
2020-09-11 17:54:50 +0800

    :new:Sitemap插件增加版权验证

--- sitemap/_controller_/sitemap/admin/Admin.php
+++ sitemap/_controller_/sitemap/admin/Admin.php
@@ -1,74 +1,77 @@
-<?php
-namespace app\plugins\sitemap\admin;
-use app\service\PluginsAdminService;
-use app\service\PluginsService;
-use think\Controller;
-/**
- * Sitemap - 后台管理
- * @author   Devil
- * @blog     http://gong.gg/
- * @version  0.0.1
- * @datetime 2016-12-01T21:51:08+0800
- */
-class Admin extends Controller
-{
-    // 后台管理入口
-    public function index($params = [])
-    {
-        $sitemap = new sitemap();
-        $data_list = $sitemap -> getSitemaps($sitemap->sitemapDir);
-        $is_enable = PluginsService::PluginsStatus('sitemap');
-        // 数组组装
-        $this->assign('data_list', $data_list['data']);
-        $this->assign('page_size', $sitemap->pageSize);
-        $this->assign('is_enable', $is_enable);
-        return $this->fetch('../../../plugins/view/sitemap/admin/admin/index');
-    }
-    // 开启插件
-    public function open()
-    {
-        if (PluginsService::PluginsStatus('sitemap'))
-        {
-            return DataReturn('插件已启动', 0);
-        }
-        $params = ['id' => 'sitemap', 'state' => 1];
-        return PluginsAdminService::PluginsStatusUpdate($params);
-    }
-    // 生成Sitemap
-    public function sitemap()
-    {
-        $sitemap = new sitemap();
-        return $this -> tip($sitemap -> generate(array('goods', 'article')), 'all');
-    }
-   
-    // 设置pagesize
-    public function set_pagesize($params = [])
-    {
-        $sitemap = new sitemap();
-        return $this -> tip($sitemap -> setPagesize($params), 'page');
-    }
-    // 删除sitemap.xml文件
-    public function del_file($params = [])
-    {
-        $sitemap = new sitemap();
-        return $this -> tip($sitemap -> delFile($params));
-    }
-    // 下载文件
-    public function down_file($params = [])
-    {
-        $sitemap = new sitemap();
-        return $this -> tip($sitemap -> downFile($params));
-    }
-    /**
-     * 提示
-     * @param type ajax|page 原页面提示/新页面提示
-     */
-    private function tip($ret, $type = 'ajax') {
-        if ($ret['error']) {
-            return $type === 'ajax' ? DataReturn($ret['error'], -100) : $this -> error($ret['error'], null, '', 2);
-        } else {
-            return $type === 'ajax' ? DataReturn('操作成功', 0) : $this -> success('操作成功', null, '', 2);
-        }
-    }
-}
+<?php
+namespace app\plugins\sitemap\admin;
+use app\service\PluginsAdminService;
+use app\service\PluginsService;
+use think\Controller;
+use app\plugins\sitemap\wga\WGA;
+
+/**
+ * Sitemap - 后台管理
+ * @author   小宇
+ * @blog     https://www.madfan.cn
+ * @version  1.0.0
+ * @datetime 2020-9-11T21:51:08+0800
+ */
+class Admin extends Controller
+{
+    // 后台管理入口
+    public function index($params = [])
+    {
+        $sitemap = new sitemap();
+        $data_list = $sitemap -> getSitemaps($sitemap->sitemapDir);
+        $is_enable = PluginsService::PluginsStatus('sitemap');
+        // 数组组装
+        $this->assign('data_list', $data_list['data']);
+        $this->assign('page_size', $sitemap->pageSize);
+        $this->assign('is_enable', $is_enable);
+        $this->assign('wga_tip', WGA::tip());
+        return $this->fetch('../../../plugins/view/sitemap/admin/admin/index');
+    }
+    // 开启插件
+    public function open()
+    {
+        if (PluginsService::PluginsStatus('sitemap'))
+        {
+            return DataReturn('插件已启动', 0);
+        }
+        $params = ['id' => 'sitemap', 'state' => 1];
+        return PluginsAdminService::PluginsStatusUpdate($params);
+    }
+    // 生成Sitemap
+    public function sitemap()
+    {
+        $sitemap = new sitemap();
+        return $this -> tip($sitemap -> generate(array('goods', 'article')), 'all');
+    }
+   
+    // 设置pagesize
+    public function set_pagesize($params = [])
+    {
+        $sitemap = new sitemap();
+        return $this -> tip($sitemap -> setPagesize($params), 'page');
+    }
+    // 删除sitemap.xml文件
+    public function del_file($params = [])
+    {
+        $sitemap = new sitemap();
+        return $this -> tip($sitemap -> delFile($params));
+    }
+    // 下载文件
+    public function down_file($params = [])
+    {
+        $sitemap = new sitemap();
+        return $this -> tip($sitemap -> downFile($params));
+    }
+    /**
+     * 提示
+     * @param type ajax|page 原页面提示/新页面提示
+     */
+    private function tip($ret, $type = 'ajax') {
+        if ($ret['error']) {
+            return $type === 'ajax' ? DataReturn($ret['error'], -100) : $this -> error($ret['error'], null, '', 2);
+        } else {
+            return $type === 'ajax' ? DataReturn('操作成功', 0) : $this -> success('操作成功', null, '', 2);
+        }
+    }
+}
 ?>
---
+++ sitemap/_controller_/sitemap/wga/WGA.souce.php
@@ -0,0 +1,54 @@
+<?php
+namespace app\plugins\sitemap\wga;
+use app\service\PluginsService;
+use think\Db;
+class WGA
+{
+    private static function wga() {
+        $root = dirname(__FILE__);
+        $config = self::config();
+        $r = @file_get_contents('https://api.os120.com/wga/verify?out_type=json&name=shopxoplugin_' . $config['base']['plugins'] . '&version=' . $config['base']['version'] . '&des=正版验证&domain=' . $_SERVER['SERVER_NAME'], false, stream_context_create(array('http' => array('method' => "GET",'timeout' => 3))));
+        if ($r) {
+            $r = json_decode($r, true);
+            if (isset($r['code']) && $r['code'] === -1) {
+                return $r['msg'];
+            }
+            if (isset($r['data']) && isset($r['data']['tip']) && !empty($r['data']['tip'])) {
+                file_put_contents($root . '/wga_tip.txt', $r['data']['tip']);
+            } else if (file_exists($root . '/wga_tip.txt')){
+                unlink($root. '/wga_tip.txt');
+            }
+        }
+        return false;
+    }
+    public static function config() {
+        return json_decode(file_get_contents(dirname(__FILE__) . '/../config.json'), true); 
+    }
+    public static function tip() {
+        if (file_exists(dirname(__FILE__) . '/wga_tip.txt')){
+            return '<div class="am-alert" data-am-alert><button type="button" class="am-close">&times;</button>' . file_get_contents(dirname(__FILE__) . '/wga_tip.txt') . '</div>';
+        }
+        return '';
+    }
+    public static function getGenerateWhereByTable($table, $key)
+    {
+        if ($key === 0) {
+            $res = self::wga();
+            if ($res) {
+                return DataReturn($res, -1);
+            }
+        }
+        switch ($table) {
+            case 'goods':
+                $where = 'is_shelves = 1';
+            break;
+            case 'article':
+                $where = 'is_enable = 1';
+            break;
+            default:
+                $where = '1';
+        }
+        return Db::name($table) -> where($where) -> order('upd_time DESC') -> select();
+    }
+}
+?>
