6f105bc0f511ae576643087c56e5375e7b1f94ae
馒头饭 <mhjlw@126.com>
2020-11-06 22:01:51 +0800

    :new:更新静态资源路径获取规则

--- optimizer/_js_/optimizer/index/style.js
+++ optimizer/_js_/optimizer/index/style.js
@@ -2,4 +2,23 @@ if (pluginOptimizerConf) {
     if (pluginOptimizerConf.availableSeoBaiduPush) {
         $.getScript(__attachment_host__ + '/static/plugins/js/optimizer/index/baidu_push.js?v=' + pluginOptimizerConf.cacheVersion)
     }
+    var pluginOptimizerRes = []
+    $('img').each(function(_, o){
+        var s = $(o).attr('src')
+        if (s.indexOf('/static/upload/images/') > -1) {
+            pluginOptimizerRes.push(s.replace(/http[s]:\/\/.*?\//g,''))
+        }
+    })
+    $('script').each(function(_, o){
+        var s = $(o).attr('src')
+        if (s.indexOf('/static/common/lib/') > -1) {
+            pluginOptimizerRes.push(s.replace(/http[s]:\/\/.*?\//g,''))
+        }
+    })
+    $('link').each(function(_, o){
+        var s = $(o).attr('href')
+        if (s.indexOf('/static/common/lib/') > -1) {
+            pluginOptimizerRes.push(s.replace(/http[s]:\/\/.*?\//g,''))
+        }
+    })
 }
