52bf974505b9a2fef5d961e4ba99201951fd3644
馒头饭 <mhjlw@126.com>
2020-11-07 22:03:18 +0800

    :new:修正资源收集引擎

--- optimizer/_js_/optimizer/index/style.js
+++ optimizer/_js_/optimizer/index/style.js
@@ -3,22 +3,12 @@ if (pluginOptimizerConf) {
         $.getScript(__attachment_host__ + '/static/plugins/js/optimizer/index/baidu_push.js?v=' + pluginOptimizerConf.cacheVersion)
     }
     var pluginOptimizerRes = []
-    $('img').each(function(_, o){
-        var s = $(o).attr('src')
-        if (s.indexOf('/static/upload/images/') > -1) {
-            pluginOptimizerRes.push(s.replace(/http[s]:\/\/.*?\//g,''))
-        }
-    })
-    $('script').each(function(_, o){
-        var s = $(o).attr('src')
-        if (s.indexOf('/static/common/lib/') > -1) {
-            pluginOptimizerRes.push(s.replace(/http[s]:\/\/.*?\//g,''))
-        }
-    })
-    $('link').each(function(_, o){
-        var s = $(o).attr('href')
-        if (s.indexOf('/static/common/lib/') > -1) {
-            pluginOptimizerRes.push(s.replace(/http[s]:\/\/.*?\//g,''))
-        }
+    $([{t: 'img', a: 'src', s: '/static/upload/images/'}]).each(function(_, c) {
+        $(c.t).each(function(_, o) {
+            var s = $(o).attr(c.a)
+                if (s && s.indexOf(c.s) > -1) {
+                    pluginOptimizerRes.push(s.replace(/http[s]:\/\/.*?\//g,'').split('?')[0])
+                }
+        })
     })
 }
