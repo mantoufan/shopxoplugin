1a16ca647f774b44a0a4268d6b37051db9e5570b
馒头饭 <mhjlw@126.com>
2020-11-10 21:50:34 +0800

    :new:更新数据美化插件配置信息

--- dataprettify/_controller_/dataprettify/Hook.php
+++ dataprettify/_controller_/dataprettify/Hook.php
@@ -40,6 +40,8 @@ class Hook extends Controller
             $goods_id = $params['data']['id'];
             $sales_count = $params['data']['sales_count'];
             $access_count = $params['data']['access_count'];
+            $plugins_dataprettify_sales_count = !empty($params['data']['plugins_dataprettify_sales_count']) ? $params['data']['plugins_dataprettify_sales_count'] : 0;
+            $plugins_dataprettify_access_count = !empty($params['data']['plugins_dataprettify_access_count']) ? $params['data']['plugins_dataprettify_access_count'] : 0;
         } else {
             return '';
         }
@@ -65,17 +67,17 @@ class Hook extends Controller
             
             if ($availables['fav']) {
                 $ret = Service::loadData($goods_id);
-                $h .= '<label>收藏数量修改</label>
+                $h .= '<label>收藏数量修改<span class="am-form-group-label-tips">真实收藏数量：' . $ret['plugins_dataprettify_fav_count_min'] . '</span></label>
                        <input type="text" name="plugins_dataprettify_fav_count" placeholder="请输入收藏数量，数量不能小于真实收藏数量（' . $ret['plugins_dataprettify_fav_count_min'] . '），不修改留空即可" min="' . $ret['plugins_dataprettify_fav_count_min'] . '" class="am-radius am-field-valid" value="' . $ret['plugins_dataprettify_fav_count'] . '">
                        <input type="hidden" name="plugins_dataprettify_fav_count_source" value="' . $ret['plugins_dataprettify_fav_count'] . '">';
             }
             if ($availables['sales']) {
-                $h .= '<label>销量修改</label>
+                $h .= '<label>销量修改<span class="am-form-group-label-tips">真实销量：' . ($sales_count - $plugins_dataprettify_sales_count) . '</span></label>
                        <input type="text" name="plugins_dataprettify_sales_count" placeholder="请输入销量，不修改留空即可" min="0" max="9999999999" class="am-radius am-field-valid" value="' . $sales_count . '">
                        <input type="hidden" name="sales_count" value="' . $sales_count . '">';
             }
             if ($availables['access']) {
-                $h .= '<label>浏览次数修改</label>
+                $h .= '<label>浏览次数修改<span class="am-form-group-label-tips">真实浏览次数：' . ($access_count - $plugins_dataprettify_sales_count) . '</span></label>
                        <input type="text" name="plugins_dataprettify_access_count" placeholder="请输入浏览次数，不修改留空即可" min="0" max="9999999999" class="am-radius am-field-valid" value="' . $access_count . '">
                        <input type="hidden" name="access_count" value="' . $access_count . '">';
             }
