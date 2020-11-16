cfb895908bb3bb2379440904db69ef14fef19a9d
馒头饭 <mhjlw@126.com>
2020-11-16 17:02:09 +0800

    :new:自定义风格

--- diystyle/_controller_/diystyle/admin/Admin.php
+++ diystyle/_controller_/diystyle/admin/Admin.php
@@ -1,63 +1,69 @@
-<?php
-namespace app\plugins\fontswitch\admin;
-
-use think\Controller;
-use app\service\PluginsService;
-
-/**
- * 字体切换 - 后台管理
- * @author   小宇
- * @blog     https://www.madfan.cn
- * @version  1.0.0
- * @datetime 2019-04-29T12:53:09+0800
- */
-class Admin extends Controller
-{
-    // 后台管理入口
-    public function index($params = [])
-    {
-        $ret = PluginsService::PluginsData('fontswitch');
-        if($ret['code'] == 0)
-        {
-            $ret['data']['code'] = htmlspecialchars_decode($ret['data']['code']);
-            $this->assign('data', $ret['data']);
-            return $this->fetch('../../../plugins/view/fontswitch/admin/admin/index');
-        } else {
-            return $ret['msg'];
-        }
-    }
-
-	/**
-     * 编辑页面
-     * @author   小宇
-     * @blog     https://www.madfan.cn
-     * @version  1.0.0
-     * @datetime 2019-04-29T12:54:01+0800
-     * @param    [array]          $params [输入参数]
-     */
-    public function saveinfo($params = [])
-    {
-        $ret = PluginsService::PluginsData('fontswitch');
-        if($ret['code'] == 0)
-        {
-            $ret['data']['code'] = htmlspecialchars_decode($ret['data']['code']);
-            $this->assign('data', $ret['data']);
-            return $this->fetch('../../../plugins/view/fontswitch/admin/admin/saveinfo');
-        } else {
-            return $ret['msg'];
-        }
-    }
-	    /**
-     * 数据保存
-     * @author   小宇
-     * @blog     https://www.madfan.cn
-     * @version  1.0.0
-     * @datetime 2019-04-29T12:54:08+0800
-     * @param    [array]          $params [输入参数]
-     */
-    public function save($params = [])
-    {
-        return PluginsService::PluginsDataSave(['plugins'=>'fontswitch', 'data'=>$params]);
-    }
-}
+<?php
+namespace app\plugins\diystyle\admin;
+
+use think\Controller;
+use app\service\PluginsService;
+use app\plugins\diystyle\wga\WGA;
+/**
+ * 自定义风格 - 后台管理
+ * @author   小宇
+ * @blog     https://www.madfan.cn
+ * @version  1.0.0
+ * @datetime 2019-04-29T12:53:09+0800
+ */
+class Admin extends Controller
+{
+    // 后台管理入口
+    public function index($params = [])
+    {
+        $ret = PluginsService::PluginsData('diystyle');
+        if($ret['code'] == 0)
+        {
+            if (!empty($ret['data']['code'])) {
+                $ret['data']['code'] = htmlspecialchars_decode($ret['data']['code']);
+            }
+            $this->assign('wga_tip', WGA::tip());
+            $this->assign('data', $ret['data']);
+            return $this->fetch('../../../plugins/view/diystyle/admin/admin/index');
+        } else {
+            return $ret['msg'];
+        }
+    }
+
+	/**
+     * 编辑页面
+     * @author   小宇
+     * @blog     https://www.madfan.cn
+     * @version  1.0.0
+     * @datetime 2019-04-29T12:54:01+0800
+     * @param    [array]          $params [输入参数]
+     */
+    public function saveinfo($params = [])
+    {
+        $ret = PluginsService::PluginsData('diystyle');
+        if($ret['code'] == 0)
+        {
+            if (!empty($ret['data']['code'])) {
+                $ret['data']['code'] = htmlspecialchars_decode($ret['data']['code']);
+            }
+            $this->assign('wga_tip', WGA::tip());
+            $this->assign('data', $ret['data']);
+            return $this->fetch('../../../plugins/view/diystyle/admin/admin/saveinfo');
+        } else {
+            return $ret['msg'];
+        }
+    }
+	/**
+     * 数据保存
+     * @author   小宇
+     * @blog     https://www.madfan.cn
+     * @version  1.0.0
+     * @datetime 2019-04-29T12:54:08+0800
+     * @param    [array]          $params [输入参数]
+     */
+    public function save($params = [])
+    {
+        return PluginsService::PluginsDataSave(['plugins'=>'diystyle', 'data'=>$params]);
+    }
+}
 ?>
---
+++ diystyle/_controller_/diystyle/wga/WGA.source.php
@@ -0,0 +1,43 @@
+<?php
+namespace app\plugins\diystyle\wga;
+use app\service\PluginsService;
+class WGA
+{
+    private static function wga() {
+        $root = dirname(__FILE__);
+        $config = self::config();
+        $_domain_ar = !empty($_SERVER['HTTP_HOST']) ? explode(':', $_SERVER['HTTP_HOST']) : array($_SERVER['SERVER_NAME']);
+        $_domain = reset($_domain_ar);
+        $r = @file_get_contents('https://api.os120.com/wga/verify?out_type=json&name=shopxoplugin_' . $config['base']['plugins'] . '&version=' . $config['base']['version'] . '&des=正版验证&domain=' . $_domain, false, stream_context_create(array('http' => array('method' => "GET",'timeout' => 3))));
+        if ($r) {
+            $r = json_decode($r, true);
+            if (isset($r['code']) && $r['code'] === -1) {
+                return $r['msg'];
+            }
+            if (isset($r['data']) && isset($r['data']['tip']) && !empty($r['data']['tip'])) {
+                file_put_contents($root . '/wga_tip.txt', $r['data']['tip']);
+            } else if (file_exists($root . '/wga_tip.txt')){
+                unlink($root. '/wga_tip.txt');
+            }
+        }
+        return false;
+    }
+    public static function config() {
+        return json_decode(file_get_contents(dirname(__FILE__) . '/../config.json'), true); 
+    }
+    public static function tip() {
+        if (file_exists(dirname(__FILE__) . '/wga_tip.txt')){
+            return '<div class="am-alert" data-am-alert><button type="button" class="am-close">&times;</button>' . file_get_contents(dirname(__FILE__) . '/wga_tip.txt') . '</div>';
+        }
+        return '';
+    }
+    public function save($params = [])
+    {
+        $res = self::wga();
+        if ($res) {
+            return DataReturn($res, -1);
+        }
+        return PluginsService::PluginsDataSave(array('plugins'=>'diystyle', 'data'=>$params));
+    }
+}
+?>
