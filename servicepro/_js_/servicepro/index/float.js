a39e94f0f690e64cb2f3f70506ce03a2c3fbb74c
馒头饭 <mhjlw@126.com>
2021-01-31 23:00:53 +0800

    :new:客服专业版1.1.1

--- servicepro/_js_/servicepro/index/float.js
+++ servicepro/_js_/servicepro/index/float.js
@@ -4,6 +4,7 @@ if (serviceproSider.length > 0) {
     // 在线客服
     $('.servicepro .btn-open').click(function()
     {
+        $('.servicepro').removeClass('servicepro-close').addClass('servicepro-open');
         $('.servicepro .content').animate({'margin-right':'0px'}, 300);
         $('.servicepro .btn-open').css('display', 'none');
         $('.servicepro .btn-ctn').css('display', 'block');        
@@ -11,6 +12,7 @@ if (serviceproSider.length > 0) {
 
     $('.servicepro .btn-ctn').click(function()
     {
+        $('.servicepro').removeClass('servicepro-open').addClass('servicepro-close');
         $('.servicepro .content').animate({'margin-right':'-150px'}, 300);
         $('.servicepro .btn-open').css('display', 'block');
         $('.servicepro .btn-ctn').css('display', 'none');  
